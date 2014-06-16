9ba0726c60586d80a40f9e123eb3cc4f8346475c
Marius Gerling <marius@gerl1ng.de>
2014-06-16 23:11:06 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 simple-gallery
 ==============
+
+This is a simple php and js gallery for serving images of different types.
+I develop this gallery for my own usage. I like to take pictures and want to show them to others.
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<title>Simple Gallery</title>
+	</head>
+	<body>
+		<?php
+			//Ordner aus galleries auslesen
+			//Wenn die Ordner eine .password Datei haben nicht auflisten
+			//in der Index.php kann man ein passwort eingeben, welches gehashed an den Server übertragen 
+			echo '<h1>Hier soll eine tolle Gallery entstehen!</h1>';
+		?>
+	</body>
+</html>
